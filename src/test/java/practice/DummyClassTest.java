b2fa9361900a9e120b9bbcd92a40a488f284607d
ratnadeep <ratnadeepkhaple5@gmail.com>
2022-09-19 20:56:39 +0530

    First commit- Engineeer1

---
+++ src/test/java/practice/DummyClassTest.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class DummyClassTest {
+
+	@Test
+	public void dummyTestMethod() {
+		System.out.println("from DummyClassTest");
+	}
+}
